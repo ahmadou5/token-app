bc849b1914d2ae1d39cf89df25e53857757d35b4
ahmadou5 <ahmadlasauwal@gmail.com>
2026-04-02 06:53:41 +0100

    refactor: remove all console.logss

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
 "use client";
 import { TokenGrid } from "@/components/TokenGrid";
-import { tokenRequest } from "@/lib/token";
-import Image from "next/image";
-import { useEffect } from "react";
 
 export default function Home() {
-  const fetchTokens = async () => {
-    const token = await tokenRequest.curatedAssetsList();
-    console.log("token", token);
-  };
-  useEffect(() => {
-    fetchTokens();
-  }, []);
   return <TokenGrid onTokenClick={(t) => console.log(t)} />;
 }
