df37700ee21b1e7e8c5f3d120db9daafcef3c63d
ahmadou5 <ahmadlasauwal@gmail.com>
2026-04-02 07:17:50 +0100

    fix: navigation

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const router = useRouter();
 
   const handleNavigate = (assetId: string) => {
-    router.push(`/tokens/${assetId}`);
+    router.push(`/token/${assetId}`);
   };
   return <TokenGrid onTokenClick={(id) => handleNavigate(id.assetId)} />;
 }
