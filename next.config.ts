0c2c49086baa6b79b0c75582cacdbb39392ea62b
ahmadou5 <ahmadlasauwal@gmail.com>
2026-04-21 17:44:29 +0100

    fix: fix server error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: [
+    "@coral-xyz/anchor",
+    "@solana-developers/helpers",
+    "@meteora-ag/m3m3",
+    "@mercurial-finance/dynamic-amm-sdk",
+  ],
 };
 
 export default nextConfig;
